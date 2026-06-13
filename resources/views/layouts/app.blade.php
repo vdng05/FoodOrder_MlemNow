12255b17d83f806316716fb68f896437dc9f5041
Vũ Đăng <101622939+vuhaidangz@users.noreply.github.com>
2026-06-13 19:29:42 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MlemNow</title>
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
